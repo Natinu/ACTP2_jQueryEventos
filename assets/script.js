25e761a1bd0ca7aca5513cbe0cdcfaafd6e29db1
nataly <natimedina.nu@gmail.com>
2018-07-19 20:40:07 -0400

    punto 3 listo

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,22 @@
 $( document ).ready(function() {
-	//Apertura tarjeta
-    $( ".card" ).click(function() {
-  		$( this ).toggleClass("card--open")
+	// Apertura tarjeta
+    $( ".card" ).click(function(e) {
+  		$( this ).toggleClass("card--open");
+  			e.preventDefault();
 	});
 
 	// Like corazón
 	$( ".card__like " ).click(function(e) {
-  		$( this ).toggleClass("card__like--red")
-  		e.preventDefault();
-
+  		$(".card__like" ).toggleClass("card__like--red");
+  			e.preventDefault();
+  			e.stopPropagation();
+  			//return false;
 	});
 
-	//
+	// Botón seguir
+	$( ".card__follow-btn" ).click(function() {
+  		$( this ).toggleClass("card__follow-btn--following");
+  			return false;
+	});
 
 });
